a91b467fda6c9de22bd69c0869cfb72311c96856
Kirill Schumilov <Kirill.Schumilov@t-systems-mms.com>
2017-04-14 11:10:20 +0300

    Added global exception handling

--- mobileOffice/src/main/java/base/controllers/BaseController.java
+++ mobileOffice/src/main/java/base/controllers/BaseController.java
@@ -1,17 +1,22 @@
 package base.controllers;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 public abstract class BaseController {
+
     @ExceptionHandler(Exception.class)
     public ModelAndView handleError(HttpServletRequest req, Exception ex) {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", ex);
-        mav.addObject("url", req.getRequestURL());
-        mav.setViewName("error");
+        if (ex.getClass() == AccessDeniedException.class){
+            mav.setViewName("403");
+        } else {
+            mav.setViewName("error");
+        }
         return mav;
     }
 }
--- mobileOffice/src/main/webapp/WEB-INF/views/403.jsp
+++ mobileOffice/src/main/webapp/WEB-INF/views/403.jsp
@@ -1,16 +1,21 @@
 <%--
   Created by IntelliJ IDEA.
   User: kisc
-  Date: 3/30/2017
-  Time: 2:40 PM
+  Date: 4/13/2017
+  Time: 11:58 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+  <link type="text/css" rel="stylesheet" href="/resources/css/error.css"/>
+  <title>Error</title>
 </head>
 <body>
-  ACCESS DENIED!
+<div class="main-content">
+  <div class="wrapper">
+    <div class="forbidden-image"></div>
+  </div>
+</div>
 </body>
 </html>
